000d91a61297742dd09932f08b6282a8dcd9ff4f
Luis Polina <luis.282001@gmail.com>
2023-06-14 11:56:02 -0600

    Pokemon card and pokemon photo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { PokemonPhotoComponent } from './components/pokemon-photo/pokemon-photo.component';
+import { PokemonCardComponent } from './components/pokemon-card/pokemon-card.component';
+import { DetailComponent } from './components/detail/detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PokemonPhotoComponent,
+    PokemonCardComponent,
+    DetailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/pokemon-photo/pokemon-photo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-photo',
+  templateUrl: './pokemon-photo.component.html',
+  styleUrls: ['./pokemon-photo.component.scss']
+})
+export class PokemonPhotoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
